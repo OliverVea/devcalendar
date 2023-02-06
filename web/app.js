4e5d354127388eef41cc328c0fe9b2ef9fd2e278
OliverVea <oliver.vea@gmail.com>
2023-02-06 01:04:25 +0100

    adding vue project

--- web/app.js
+++
@@ -1,9 +0,0 @@
-async function getDataFromPython() {
-    var dates = await eel.get_date_details('2022-11-20')();
-    console.log(dates)
-    document.getElementById('header').innerText = dates;
-}
-
-document.getElementById('button').addEventListener('click', () => {
-    getDataFromPython();
-})
